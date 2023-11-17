8634350976d8607f06f3cd756758edf0728c3bad
Tanmay Sahu <sahutanmay1903@gmail.com>
2023-11-18 00:10:26 +0530

    'update'

--- app.js
+++ app.js
@@ -6,43 +6,63 @@ const image = document.querySelector("img");
 const description = document.querySelector("#description");
 
 const updateWeather = (city) => {
-    if (city === "") {
-    image.src="images/404.png"
-    temperature.innerHTML = ``;
-    windSpeed.innerHTML =  ``;
-    humidity.innerHTML =``;
-    description.innerHTML = ``;       
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${ApiKey}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.cod === "404") {
+        image.src = "images/404.png";
+        image.style.height = "230px";
+        temperature.innerHTML = ``;
+        description.innerHTML = `location not found!`;
+        document.getElementById("b").style.display = "none";
+      } else {
+        switch (json.weather[0].main) {
+          case "Clear":
+            image.src = "images/clear.png";
+            break;
+          case "Rain":
+            image.src = "images/rain.png";
+            break;
+          case "Clouds":
+            image.src = "images/cloud.png";
+            break;
+          case "Snow":
+            image.src = "images/snow.png";
+            break;
+          case "Mist":
+          case "Haze":
+            image.src = "images/mist.png";
+            break;
+          default:
+            image.src = "images/cloud.png";
+        }
 
-    }
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${ApiKey}`)
-        .then(response => response.json())
-        .then(json => {
-           
-
-            switch(json.weather[0].main){
-                case 'Clear':image.src="images/clear.png";break;
-                case 'Rain':image.src="images/rain.png";break;
-                case 'Clouds':image.src="images/cloud.png";break;
-                case 'Snow':image.src="images/snow.png";break;
-                case 'Mist':image.src="images/mist.png";break;
-                case 'Haze':image.src="images/mist.png";break;
-                default:image.src = "images/cloud.png";
-            }
-
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span><sup>o</sup>C</span>`;
-            windSpeed.innerHTML =  `${parseInt(json.wind.speed)}km/hr`;
-            humidity.innerHTML =`${json.main.humidity}%`;
-            description.innerHTML = `${json.weather[0].description}`;
-        });
+        temperature.innerHTML = `${parseInt(
+          json.main.temp
+        )}<span><sup>o</sup>C</span>`;
+        windSpeed.innerHTML = `${parseInt(json.wind.speed)}km/hr`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        description.innerHTML = `${json.weather[0].description}`;
+        document.getElementById("b").style.display = "flex";
+        image.style.height = "170px";
+        document.querySelector("input").value="";
+      }
+    });
 };
 
-document.querySelector("button").addEventListener('click', () => {
+document.querySelector("button").addEventListener("click", () => {
+  const city = document.querySelector("input").value;
+  updateWeather(city);
+});
+document.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
     const city = document.querySelector("input").value;
     updateWeather(city);
+  }
 });
-
-document.querySelector("input").addEventListener('keyup', () => {
-    const city = document.querySelector("input").value;
-    updateWeather(city);
-});
+// document.querySelector("input").addEventListener("keyup", () => {
+//  const city = document.querySelector("input").value;
+//  updateWeather(city);
+// });
